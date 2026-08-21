0f1899704c1dc05fe29b78818f436102428cd0eb
Majkii2006 <michaldoa08@gmail.com>
2026-08-21 13:07:34 +0200

    random replacement cache policy in progress

---
+++ 012_RRCache/src/main.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <random>
+#include <unordered_map>
+#include <utility>
+
+struct Element {
+
+	Element(int key, int value) : m_key(key), m_value(value) {
+		std::cout << "Element created" << std::endl;
+	}
+
+	~Element() {
+		std::cout << "Element deleted" << std::endl;
+	}
+
+	int m_key { };
+	int m_value { };
+	
+};
+
+class RRCache {
+	private:
+		int pickRandomly() {
+			//wylosowac randomowo indeks wzakresie od  elementu do m_capacity	
+			std::mt19937 gen(rd());
+			std::uniform_int_distribution<> distribution(0, static_cast<int>(m_capacity) - 1);
+			return distribution(gen);
+			
+		}
+
+	public:
+		RRCache(int capacity) : m_capacity(capacity) {
+			std::cout << "RRCache instance created" << std::endl;
+		}
+		
+		~RRCache() {
+			std::cout << "RRCache instance deleted" << std::endl;
+			for (auto it { map.begin() }; it != map.end(); ++it) {
+				if (it->second != nullptr) {
+					delete it->second;
+				}	
+			}	
+		}
+
+
+		int get(int key) const {
+			if (map.contains(key)) {
+				Element* element = map.at(key);
+				return element->m_value;
+			}			
+			return -1;
+		}
+
+		void put(int key, int value) {
+			if (!(map.contains(key))) {
+				if (map.size() < m_capacity) {
+					Element* newElement = new Element(key, value);	
+					map.emplace(key, newElement);
+				} 
+				else {
+					int index_to_delete = pickRandomly();
+					for ( auto it { map.begin() }; it != map.end(); ++it) {
+						index_to_delete--;									
+						if (index_to_delete < 0) {
+							map.erase(it);		
+							Element* toDelete = it->second;
+							delete toDelete;
+							return;
+						}	
+					}		
+				}
+
+			} else {
+				Element* toModify = map.at(key);						
+				toModify->m_value = value;
+			} 
+		}
+
+	private:
+		std::size_t m_capacity { };
+		std::unordered_map<int, Element*> map { };
+		std::random_device rd;
+};
+
+
+
+
+int main() {
+	RRCache instance(2);
+	instance.put(1, 1);
+	instance.put(2, 2);
+	instance.put(3, 3);
+
+
+	return 0;
+}
