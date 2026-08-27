527bbc51d24c171e829c35cbe329f4f0f96dab89
Majkii2006 <michaldoa08@gmail.com>
2026-08-27 23:22:37 +0200

    creating the cache factory using factory design pattern

---
+++ CacheFactory/src/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#include "../include/LRUCache.hpp"
+
+class Cache {
+
+	public:
+		auto createCache() {
+				
+		}
+
+	private:
+		Cache() {
+			std::cout << "Cache Constructed" << std::endl;
+		}
+		virtual ~Cache() {};	
+};
+
+
+
+
+int main() {
+
+
+
+
+}
